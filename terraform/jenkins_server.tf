74f910ef4f1edb42bef270c5b3c20e9339e3feee
Osakpolor Courage Ihensekhien <osascourage27@gmail.com>
2023-03-09 16:41:37 +0100

    add terraform config

---
+++ terraform/jenkins_server.tf
@@ -0,0 +1,56 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Define the VPC and subnet to launch the instance in
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.vpc.id
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# Define the security group to allow SSH access
+resource "aws_security_group" "ssh_sg" {
+  name_prefix = "ssh_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Define the EC2 instance with userdata
+resource "aws_instance" "jenkins_instance" {
+  ami           = "ami-05b457b541faec0ca"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
+  tags = {
+    Name = "jenkins_instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update 
+              sudo apt install -y docker.io &&
+              docker run -p 8080:8080 -p 50000:50000 -d \ 
+              -v jenkins_home:/var/jenkins_home \
+              -v /var/run/docker.sock:/var/run/docker.sock \
+              -v $(which docker):/usr/bin/docker jenkins/jenkins:lts
+              EOF
+}
