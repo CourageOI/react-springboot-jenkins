323dea48703cbf470aa8c33c1d5069d6ac312466
Osakpolor Courage Ihensekhien <osascourage27@gmail.com>
2023-03-10 04:21:47 +0100

    edit userdata

--- terraform/jenkins_server.tf
+++ terraform/jenkins_server.tf
@@ -79,5 +79,13 @@ resource "aws_instance" "jenkins_instance" {
     Name = "jenkins_instance"
   }
 
-  user_data = file("user_data.sh")
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update 
+              sudo apt install -y docker.io &&
+              docker run -p 8080:8080 -p 50000:50000 -d \ 
+              -v jenkins_home:/var/jenkins_home \
+              -v /var/run/docker.sock:/var/run/docker.sock \
+              -v $(which docker):/usr/bin/docker jenkins/jenkins:lts
+              EOF
 }
