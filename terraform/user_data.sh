50653aae93d3bbf28a0d952edd1bafe6add787b2
Osakpolor Courage Ihensekhien <osascourage27@gmail.com>
2023-03-13 12:27:45 +0100

    update to user_data

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update && sudo apt upgrade -y
 sudo apt install default-jre -y
 sudo apt install jenkins -y
-sudo systemctl start jenkins
+sudo systemctl start jenkins.service
